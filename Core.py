baf73f47c67c38662eede57f82b1f0045c91c7c6
Local-Bird <john121803@gmail.com>
2024-04-27 00:21:46 -0400

    chng binarize(), fast.py
    binarize() now uses OTSU'S instead of GAUSSIAN, fast.py now checks if something is dark or light before preprocessing it, as this generally detects whether something is a document or screenshot. Fast.py can discriminate between speeds for users to QA the OCR output before saving.

--- Core.py
+++ Core.py
@@ -2,6 +2,7 @@
 from PIL import Image
 import cv2
 import pytesseract
+import numpy as np
 
 # BASIC WORKFLOW: 
     # Open an image using Pillow
@@ -24,23 +25,36 @@ def invert(img):
 def binarize(img):
     process = "binarize"
     binarize_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    binarize_image = cv2.adaptiveThreshold(binarize_image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+    #binarize_image = cv2.adaptiveThreshold(binarize_image, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
+    #thresh, binarize_image = cv2.threshold(binarize_image, 210, 230, cv2.THRESH_BINARY)
+    blur = cv2.GaussianBlur(binarize_image,(5,5),0)
+    thresh,binarize_image = cv2.threshold(binarize_image,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+
+
     cv2.imwrite("temp/binarize.jpg", binarize_image)
     temp_file = f'temp/{process}.jpg'
-    temp = cv2.imread(temp_file)
-    #cv2.imshow("Binarize Image", temp)
-    #cv2.waitKey(1000)
+    img = cv2.imread(temp_file)
     return img
 
 def xnoise(img):
     process = "xnoise"
-    xnoise_image = cv2.fastNlMeansDenoising(img, 7, 21)
-    cv2.imwrite("temp/xnoise.jpg", xnoise_image)
+    kernel = np.ones((2, 2), np.uint8)
+    img = cv2.dilate(img, kernel, iterations=2)
+    kernel = np.ones((1, 1), np.uint8)
+    img = cv2.erode(img, kernel, iterations=1)
+    img = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
+    img = cv2.medianBlur(img, 1)
+
+    img = cv2.bitwise_not(img)
+    kernel = np.ones((1,1),np.uint8)
+    img = cv2.dilate(img, kernel, iterations=2)
+    img = cv2.bitwise_not(img)
+    
+    cv2.imwrite("temp/xnoise.jpg", img)
     temp_file = f'temp/{process}.jpg'
     temp = cv2.imread(temp_file)
-    #cv2.imshow("Xnoise Image", temp)
-    #cv2.waitKey(1000)
-    return img
+    
+    return (img)
 
 
 
@@ -58,8 +72,10 @@ def xnoise(img):
 
 def main():
     process=''
+    language = input("What language are you using?")
+    language = language[0:3].lower()
     image_name = input("What is the name of your image?")
-    image_file = (f"ImagesIn/{image_name}")
+    image_file = (f"ImagesIn{language}/{image_name}")
     temp_file = f'temp/{process}.jpg'
     img = cv2.imread(image_file)
     #cv2.imshow("original image", img)
@@ -67,31 +83,35 @@ def main():
 
     Qinput = input('Would you like to invert your image? y/n ')
     if Qinput == 'y':
-        invert(img)
+        img = invert(img)
     else:
         pass
     # Binarize images with OpenCV
 
     Qgray = input('Would you like to binarize your image? y/n ')
     if Qgray == 'y':
-        binarize(img)
+        img = binarize(img)
+        cv2.imshow("Binarize Image", img)
+        cv2.waitKey(0)
 
     # Remove noise from images with OpenCV
 
     Qnoise = input("Would you like to remove noise from your image? y/n ")
     if Qnoise == 'y':
-        xnoise(img)
-        binarize(img)
-
+        img = xnoise(img)
+        cv2.imshow("Xnoise Image", img)
+        cv2.waitKey(0)
     #Put the modified image through Pytesseract to OCR it!
     QOCR = input("Would you like to OCR your image? y/n ")
     if QOCR == 'y':
-        ocr_result = pytesseract.image_to_string(img)
+        #cv2.imshow("Binarize Image", img)
+        #cv2.waitKey(0)
+        ocr_result = pytesseract.image_to_string(img,lang=f'{language}')
         
         print(ocr_result)
         QLike = input("Did you like the result? y/n ")
         if QLike == 'y':
-            text_file = open(f'OCR_Text\{image_name}.txt', 'w')
+            text_file = open(f'OCR_Text\{image_name}.txt', 'w', encoding='utf-8')
             text_file.write(ocr_result)
         pass
 
--- Fast.py
+++ Fast.py
@@ -2,20 +2,33 @@ from PIL import Image
 import cv2
 import pytesseract
 import os
-from Core import invert, binarize
+from Core import invert, binarize, xnoise
 language = input("What language are you using?")
 language = language[0:3].lower()
-
+speed = input("How fast would you like to go? Options: 'fast' or 'superfast'")
 directory = r"C:\Users\john1\OneDrive\Desktop\CS32Project\TesRepository3\ImagesIn"+f'{language}'
 
 for filename in os.listdir(directory):
     image_file = (f"ImagesIn{language}/{filename}")
+    temp = Image.open(image_file)
+    test = temp.getpixel((1,1))
     img = cv2.imread(image_file)
+    wb = test[0] + test[1] + test[2]
+    print(wb)
 
-    invert(img)
-    binarize(img)
+    if wb > 300:
+        img = invert(img)
+        img = binarize(img)
 
     ocr_result = pytesseract.image_to_string(img,lang=f'{language}')
-    print(ocr_result)
-    text_file = open(f'OCR_Text\{filename}.txt', 'w', encoding='utf-8')
-    text_file.write(ocr_result)
+    print(f"THIS IS {filename}", ocr_result)
+    if speed == 'fast':
+        save = input("Did you like the result?")
+        if save == 'y':
+            text_file = open(f'OCR_Text\{filename}.txt', 'w', encoding='utf-8')
+            text_file.write(ocr_result)
+        else:
+            pass
+    if speed == 'superfast':
+        text_file = open(f'OCR_Text\{filename}.txt', 'w', encoding='utf-8')
+        text_file.write(ocr_result)
